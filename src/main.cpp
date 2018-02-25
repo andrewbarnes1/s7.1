0834f87eb96b52fccd4efe1138d0067f24ba9907
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2018-02-25 23:20:31 +0000

    display message on LCD

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,34 @@
 #include <mbed.h>
 #include <EthernetInterface.h>
 #include <rtos.h>
-//#include <C12832.h>
 
-//C12832 lcd(D11, D13, D12, D7, D10);
+#include <C12832.h>
+char buffer[1024]; /* 1k bytes */
+
 int main() {
+    C12832 lcd(D11, D13, D12, D7, D10);
     EthernetInterface eth;
     UDPSocket udp;
 
-    printf("starting\n");
-    //lcd.locate(0, 0);
-    //lcd.printf("conecting \n");
-    printf("conecting \n");
-
+    printf("conecting on DHCP\n");
     eth.connect();
     const char *ip = eth.get_ip_address();
     printf("IP address is: %s\n", ip ? ip : "No IP");
 
-//    lcd.locate(0, 0);
-//    lcd.printf("IP address is: %s ", ip ? ip : "No IP");
-
-    /* Open UDP socket and bind to port number to listen to */
     udp.open( &eth);
+
+    /* Listen for UDP on port 65000 */
     udp.bind(65000);
+
     printf("listening on 65000\n");
-//    lcd.printf(" on port 65000");
 
     while(1){
-        char buffer[4*1024];/*4K bytes */
         SocketAddress source;
+        int len = udp.recvfrom( &source, buffer, sizeof(buffer));
+        buffer[len]='\0';
 
-        /* blocking IO waits for Datagram */
-        int l = udp.recvfrom( &source, buffer, sizeof(buffer));
-
-        /* write in zero-byte as string terminator */
-        buffer[l]='\0';
-
-        printf("from %s\n", source.get_ip_address());
-        printf("  at %d\n", source.get_port());
-        printf("data %s\n", buffer);
-
-//        lcd.locate(0,0);
-//        lcd.printf("from %s/%d\n", source.get_ip_address(), source.get_port());
-//        lcd.printf("\"%s\"",  buffer);
-
+        printf("message from %s/%d \n", source.get_ip_address(), source.get_port());
+        lcd.locate(0,0);
+        lcd.printf("\"%s\"", buffer);
     }
 }
